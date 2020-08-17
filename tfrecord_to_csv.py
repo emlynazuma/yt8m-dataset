8bd1fa0b98d61c400980fd577af09a129d723aa5
tongwai <anthony@voicetube.com>
2020-08-17 14:53:59 +0800

    [chore] rename

---
+++ tfrecord_to_csv.py
@@ -0,0 +1,28 @@
+import os
+from argparse import ArgumentParser
+
+import tensorflow as tf
+
+from utils import tfrecord_to_csv
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument('--category', type=str)
+    args = parser.parse_args()
+    category = args.category
+
+    data_dir = './yt8m_data_raw/'
+    log = open('./parsed.txt').read().split()
+    print(log)
+
+    for fil in os.listdir(data_dir):
+        print(fil)
+
+        if fil.endswith('tfrecord') and fil.startswith(category) and fil not in log:
+
+            with open('./parsed.txt', 'a+') as f:
+                f.write(f'{fil}\n')
+
+            dataset = tf.data.TFRecordDataset(f"{data_dir}{fil}")
+
+            tfrecord_to_csv(dataset, category)
